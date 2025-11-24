814579d1d4f7c077705536660c4f0710020d8a4c
xbkv <one.two.o.two@icloud.com>
2025-11-24 21:41:26 +0900

    [change]: TypeScriptに変更

---
+++ models/Post.ts
@@ -0,0 +1,18 @@
+import { getDB } from "../lib/MongoDB";
+
+export interface IPost {
+    count: number;
+    uniqueId: string;
+    author: string;
+    authorName?: string;
+    content?: string;
+    url?: string;
+    imageUrl?: string;
+    timestamp?: Date;
+}
+
+export const Post = {
+    collection: () => getDB().collection<IPost>("posts"),
+    findByUniqueId: (uid: string) => Post.collection().findOne({ uniqueId: uid }),
+    create: (data: IPost) => Post.collection().insertOne({ ...data, timestamp: new Date() }),
+}

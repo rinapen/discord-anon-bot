0cbfb3ce12b7b465a367b76f84019097a9dd6eaa
xbkv <one.two.o.two@icloud.com>
2025-11-24 16:52:46 +0900

    [add]: TypeScript移行のためのファイル追加 TypeScript移行のためにtsファイルを作成 index.tsにまとめられていたクライアントシステムを分割

---
+++ bot/client.ts
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+
+export const client = new Client({
+    intents: [
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages
+    ], 
+    partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.ThreadMember,
+        Partials.User
+    ]
+});

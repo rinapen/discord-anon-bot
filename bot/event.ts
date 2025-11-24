fb04607625c86b374729216ec90d90f133ba9f56
xbkv <one.two.o.two@icloud.com>
2025-11-24 17:35:15 +0900

    [add]: ClientReadyEvent関数を作成 initialize数を追加

---
+++ bot/event.ts
@@ -0,0 +1,13 @@
+import { config } from "../config";
+import { connectMongoDB } from "../lib/MongoClient";
+import { client } from "./client";
+
+export async function initialize() {
+    if (config.MONGODB_URI) {
+        await connectMongoDB(config.MONGODB_URI!);
+    } else {
+        console.log("[×] MONGODB_URIが設定されていません。")
+    }
+    client.user?.setActivity({ name: 'Created by rinapen'});
+    console.log(`[✓] ログインに成功しました: [${client.user?.tag}]`);
+}
